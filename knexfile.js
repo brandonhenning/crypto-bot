cc659e30abc7ce53abddab438c90512343b1018d
Brandon Henning <brandonhenning@Brandons-MacBook-Pro-2.local>
2018-04-19 10:27:32 -0600

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/tower-bot'
+  },
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`
   }
-
 }
+
